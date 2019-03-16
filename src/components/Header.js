807aa93cc47ad71ebd13883ef86c671746941ee5
jeffyjkang <jeffyjkang@gmail.com>
2019-03-15 18:30:16 -0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,92 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fab)
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-diamond"></span>
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-laptop" />
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Jeffrey Kang</h1>
+        <p>
+          Software Engineer
+          <br />
+          Full-Stack Web Developer
+        </p>
+        <div>
+          <div className="icons first-row">
+            <FontAwesomeIcon size="2x" icon={['fab', 'git']} />
+            <FontAwesomeIcon size="2x" icon={['fab', 'html5']} />
+            <FontAwesomeIcon size="2x" icon={['fab', 'css3']} />
+            <FontAwesomeIcon size="2x" icon={['fab', 'less']} />
+          </div>
+          <div className="icons second-row">
+            <FontAwesomeIcon size="2x" icon={['fab', 'js']} />
+            <FontAwesomeIcon size="2x" icon={['fab', 'react']} />
+            <FontAwesomeIcon size="2x" icon={['fab', 'node-js']} />
+            <FontAwesomeIcon size="2x" icon={['fab', 'python']} />
+          </div>
         </div>
-        <div className="content">
-            <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Intro
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Projects
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onOpenArticle: PropTypes.func,
-    timeout: PropTypes.bool
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
 }
 
 export default Header
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,201 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+import ice from '../images/ice.jpg'
+import water from '../images/water.jpg'
+import rock from '../images/rock.jpg'
 
 class Main extends React.Component {
   render() {
-
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      />
+    )
 
     return (
-      <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+      <div
+        ref={this.props.setWrapperRef}
+        id="main"
+        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <article
+          id="intro"
+          className={`${this.props.article === 'intro' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Introduction</h2>
+          <span className="image main">
+            <img className="introImage" src={ice} alt="" />
+          </span>
+          <p>
+            Full-Stack Web Developer and software engineer. Specializing in
+            technical operations, management and customer service forged through
+            10+ years in various fields of work.
+          </p>
+          <p>
+            Exhibits an optimistic perspective, enthusiasm and eagerness to
+            help. Advocates synergy with associates to prioritize collective
+            accolades above personal achievements. Possesses a strong conviction
+            for learning to develop and improve invariably.
+          </p>
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+        <article
+          id="work"
+          className={`${this.props.article === 'work' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">My Projects</h2>
+          <span className="image main">
+            <img src={water} alt="" />
+          </span>
+          <h5> Airfitness </h5>
+          <p>
+            <strong> Description: </strong> Airfitness allows instructors to
+            create exercise programs with a flexible location. The instructor is
+            capable of categorizing the exercise, such as yoga, weight lifting,
+            crossfit, etc. App allows users to purchase punch-cards for the
+            specified exercise, and can easily filter by location or type. I was
+            the team lead and front-end architect.
+            <br />
+            <strong> Tech-Stack: </strong> HTML, Css, JQuery, React,
+            Node.js/Postgress
+            <br />
+            <strong> # of people involved: </strong> 3
+            <br />
+            <strong> Link: </strong>{' '}
+            <a target="blank" href="https://af.netlify.com/">
+              AirFitness
+            </a>
+          </p>
+          <h5> Artfolio </h5>
+          <p>
+            <strong> Description: </strong> Portfolio app for an artist to
+            display his collection of photos. Allows a users to login and
+            comment on posts provided by the artist. Took on the role of team
+            lead for the project. My position required me to review and test the
+            group's code, troubleshoot, provide feedback and help group members
+            when they were stuck on problems.
+            <br />
+            <strong> Tech-stack: </strong> HTML, Less/Css, React,
+            Node.js/Postgress, Continuous Integration
+            <br />
+            <strong> # of people involved: </strong> 4
+            <br />
+            <strong> Link: </strong>{' '}
+            <a target="blank" href="https://artfolio-landing.netlify.com/">
+              ArtFolio
+            </a>
+          </p>
+          <h5>Flytelog</h5>
+          <p>
+            <strong> Description: </strong> Pilot log book application that
+            allows a pilot to use an online resource to log flight hours instead
+            of the traditional pen and paper format. Connects with various APIs
+            and tools that allows a user to store images, track flight routes
+            with maps, update instructor relationships, and purchase
+            memberships. I was heavily involved in designing the front-end and
+            back-end, as well as hooking up our app with the various APIs.
+            <br />
+            <strong> Tech-stack: </strong> React, Node, SQL, Material-UI. APIs:
+            Firebase-auth, Firebase-storage, Stripe for billing
+            <br />
+            <strong> # of people involved: </strong> 5
+            <br />
+            <strong> Link: </strong>{' '}
+            <a target="blank" href="https://lab9-flightlogs.firebaseapp.com/">
+              FlyteLog
+            </a>
+          </p>
+          <h5>JeffsNotes</h5>
+          <p>
+            <strong> Description: </strong>A note taking application that allows
+            users to create multiple notes. Capable of listing the various
+            notes, or a single note. Updating and deletion of notes is possible
+            as well as a search functionality to filter notes by name. User can
+            also register or login to keep track of their previous notes.
+            <br />
+            <strong> Tech-stack: </strong> React, Node.js/SQL
+            <br />
+            <strong> # of people: </strong> 1
+            <br />
+            <strong> Link: </strong>{' '}
+            <a
+              target="blank"
+              href="https://suspicious-hugle-5f1b77.netlify.com/"
+            >
+              JeffsNotes
+            </a>
+          </p>
           {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+        <article
+          id="about"
+          className={`${this.props.article === 'about' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">About Me</h2>
+          <span className="image main">
+            <img src={rock} alt="" />
+          </span>
+          <p>
+            I consider myself to be immensely fortunate to have found something
+            I am sincerely passionate about. I also realize there are many
+            people who do not share my excitement and attitude about their field
+            of study. Therefore I pledge to never idle and take for granted the
+            opportunities that are given to me. After being involved in a few
+            different divisions of employment, including management, logistics,
+            hardware technician, sales and purchasing, I found software
+            engineering and computer science. For me, a flood gate was opened
+            and I quickly became enthralled in the world of discovering new tech
+            and creating software. I can honestly and wholeheartedly say I have
+            never been as disciplined and focused about software tech compared
+            to anything else in my life. I plan to continue learning
+            indefinitely and have created schedules to adhere to this. This
+            includes research of new tech, algorithms, data structures,
+            revision, review and creating projects.
+          </p>
+          <p>
+            Growing up I was always curious about finding truths and I am still
+            the type of person to be constantly google-ing or researching about
+            anything and everything. I have always wanted to be able to make an
+            impact on a global scale and I feel the path I am on now will allow
+            for this. Another feature that I find immensely rewarding is the
+            intellectual stimulation that comes along with learning about new
+            technologies, libraries and frameworks.
+          </p>
+          <p>
+            I am elated to have found and continue to be a part of Lambda School
+            which was the vehicle that allowed me to push my knowledge and help
+            develop me into a full-stack web engineer. I am passionate about
+            every aspect, from designing to front-end and back-end. I now have
+            so many tools at my disposable but I feel the greatest tool that
+            they provided me was the ability to self-learn and the confidence to
+            be able to learn anything no matter how intimidating it may be at
+            first.{' '}
+          </p>
           {close}
         </article>
 
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article
+          id="contact"
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Contact</h2>
           <form method="post" action="#">
             <div className="field half first">
@@ -49,22 +208,43 @@ class Main extends React.Component {
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea name="message" id="message" rows="4" />
             </div>
             <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li>
+              <a href="#" className="icon fa-file-text-o">
+                <span className="label">Resume</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/jeffrey-kang-4937b117a/"
+                className="icon fa-linkedin"
+              >
+                <span className="label">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/jeffyjkang"
+                className="icon fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>Email: Jeffyjkang@gmail.com</li>
           </ul>
           {close}
         </article>
-
       </div>
     )
   }
